7eec1e1fa9c988400501b93c096f5b5c9203c395
Elvira <e.y.volobuieva@student.khai.edu>
2025-12-20 12:51:51 +0200

    Query9

--- Program.cs
+++ Program.cs
@@ -217,9 +217,9 @@ namespace Practice_Linq
                 .Where(x => x.Tournament == "UEFA Euro" && x.Date.Year == 2012 && x.Country == "Ukraine")
                 .Select(x => new
                     {
-                        x.Date.Year,
-                        x.Home_team,
-                        x.Away_team,
+                        MatchYear = x.Date.Year,
+                        Team1 = x.Home_team,
+                        Team2 = x.Away_team,
                         Goals = x.Home_score + x.Away_score
                     }
                 );
@@ -232,7 +232,7 @@ namespace Practice_Linq
 
             foreach (var game in selectedGames)
             {
-                Console.WriteLine($"{game.Year} {game.Home_team} - {game.Away_team}, Goals: {game.Goals}");
+                Console.WriteLine($"{game.MatchYear} {game.Team1} - {game.Team2}, Goals: {game.Goals}");
             }
         }
 
@@ -243,7 +243,15 @@ namespace Practice_Linq
             //Query 9: Перетворити всі матчі UEFA Nations League у 2023 році на матчі з наступними властивостями:
             // MatchYear - рік матчу, Game - назви обох команд через дефіс (першою - Home_team), Result - результат для першої команди (Win, Loss, Draw)
 
-            var selectedGames = games; // допиши запит
+            var selectedGames = games
+                .Where(x => x.Tournament == "UEFA Nations League" && x.Date.Year == 2023)
+                .Select(x => new
+                {
+                    MatchYear = x.Date.Year,
+                    Game = $"{x.Home_team} - {x.Away_team}",
+                    Result = (x.Home_score > x.Away_score) ? "Win" : (x.Home_score < x.Away_score) ? "Loss" : "Draw"
+                }
+                );
 
 
             // Результат
@@ -251,6 +259,10 @@ namespace Practice_Linq
 
             //foreach
 
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.MatchYear} {game.Game}, Result for team1: {game.Result}");
+            }
         }
 
         // Запит 10
